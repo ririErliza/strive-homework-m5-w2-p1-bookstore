068d88d685dcd0ee95981a6b708f2cdad840b644
ririErliza <ryantiadam@yahoo.co.uk>
2022-04-13 12:01:08 +0200

    update
    change the type of components for SingleBook & BookList

--- project-two/src/components/BookList.jsx
+++ project-two/src/components/BookList.jsx
@@ -1,21 +1,35 @@
-import { Component } from "react";
+// import { Component } from "react";
 import { Container, Row } from "react-bootstrap";
 import books from "../data/Books.json"
 import SingleBook from "./SingleBook";
 
-class BookList extends Component{
-    render(){
-        return(
-            <Container>
-                <Row>
-                    {
-                        books.map(book=>(<SingleBook key={book.asin} book={book}/> ))
-                    }
-                </Row>
-            </Container>
-        )
-    }
+// class BookList extends Component{
+//     render(){
+//         return(
+//             <Container>
+//                 <Row>
+//                     {
+//                         books.map(book=>(<SingleBook key={book.asin} book={book}/> ))
+//                     }
+//                 </Row>
+//             </Container>
+//         )
+//     }
         
-}
+// }
+
+const BookList = () =>(
+    <Container>
+        <Row>
+            {
+            books.map(book=>(
+            <SingleBook key={book.asin} book={book}/> 
+            ))
+            }
+        </Row>
+    </Container>
+)
+
+
 
 export default BookList
--- project-two/src/components/SingleBook.jsx
+++ project-two/src/components/SingleBook.jsx
@@ -1,18 +1,35 @@
 import { Col, Card } from 'react-bootstrap'
+import { Component } from 'react'
 
-const SingleBook = ({book}) =>
-(
-<Col xs={6} md={3} className="mb-3">
-                <Card className="h-100" key={book.asin}>
-                    <Card.Img variant="top" src={book.img} className="h-75"/>
+// const SingleBook = ({book}) =>
+// (
+// <Col xs={6} md={3} className="mb-3">
+//                 <Card className="h-100" key={book.asin}>
+//                     <Card.Img variant="top" src={book.img} className="h-75"/>
+//                     <Card.Body>
+//                         <Card.Title className="text-truncate">{book.title}</Card.Title> 
+//                     </Card.Body>
+                    
+//                 </Card>
+//             </Col>
+// )
+
+class SingleBook extends Component{
+    render(){
+        return(
+            <Col xs={6} md={3} className="mb-3">
+                <Card className="h-100" key={this.props.book.asin}>
+                    <Card.Img variant="top" src={this.props.book.img} className="h-75"/>
                     <Card.Body>
-                        <Card.Title className="text-truncate">{book.title}</Card.Title> 
+                        <Card.Title className="text-truncate">{this.props.book.title}</Card.Title> 
                     </Card.Body>
                     
                 </Card>
             </Col>
-)
             
-
+            )
+        }
+            
+    }
 
 export default SingleBook
