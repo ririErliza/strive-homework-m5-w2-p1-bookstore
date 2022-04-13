fd564cd49c5ba94112bf730f5bb97bf7fd84e6ee
ririErliza <ryantiadam@yahoo.co.uk>
2022-04-13 12:02:46 +0200

    Update SingleBook.jsx

--- project-two/src/components/SingleBook.jsx
+++ project-two/src/components/SingleBook.jsx
@@ -1,18 +1,6 @@
 import { Col, Card } from 'react-bootstrap'
 import { Component } from 'react'
 
-// const SingleBook = ({book}) =>
-// (
-// <Col xs={6} md={3} className="mb-3">
-//                 <Card className="h-100" key={book.asin}>
-//                     <Card.Img variant="top" src={book.img} className="h-75"/>
-//                     <Card.Body>
-//                         <Card.Title className="text-truncate">{book.title}</Card.Title> 
-//                     </Card.Body>
-                    
-//                 </Card>
-//             </Col>
-// )
 
 class SingleBook extends Component{
     render(){
